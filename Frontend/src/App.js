3ff47c82df29aba2a39aff4697a2886bc1e7d93c
Gehan Fonseka <gehanfonseka@gmail.com>
2024-08-23 00:18:30 +0530

    update app.js
    without appointmentform.js & css and salonhome

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/home/Header';
 import HeroSection from './Components/home/HeroSection';
+import AppointmentForm from './Components/salon/AppointmentForm';
 
 import Salonhome from './Components/salon/Salonhome';
+import ServiceList from './Components/salon/ServiceList';
+import MyAppointmentForm from './Components/salon/MyAppointmentForm';
 import Footer from './Components/home/Footer';
+
 
 
 function App() {
@@ -18,6 +22,11 @@ function App() {
       
       <Route path="/" element={<HeroSection/>} />
       <Route path="/salon" element={<Salonhome/>} />
+      <Route path="/ServiceList" element={<ServiceList />} />
+      <Route path="/AppointmentForm" element={<AppointmentForm />} />
+      <Route path="/MyAppointmentForm" element={<MyAppointmentForm />} />
+
+      
       
       </Routes>
    <Footer />
