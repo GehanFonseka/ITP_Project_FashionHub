cde6df63a790998938cc63db1283777eb3b7b297
Kavishka Deshan <kavishkadeshan428@gmail.com>
2024-08-30 11:27:36 +0530

    delete backend

--- Backend/models/userSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    name:{type:String},
-    UId: {type:String}
-});
-
-module.exports = mongoose.model("User",UserSchema);
