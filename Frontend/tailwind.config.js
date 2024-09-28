01a41a085f829c67ea5e9292d7192aedd1b8bce7
ChamuHimasha <chamuhimasha29471@gmail.com>
2024-09-28 20:37:33 +0800

    tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    theme: {
-      extend: {
-        fontFamily: {
-          russo: ["Russo One", "sans-serif"],
-          saira: ["Saira", "sans-serif"],
-        },
-        colors: {
-          primary: "#E76F51",
-          secondary: "#5C646C",
-          light: "#F4F4F4",
-          dark: "#212529",
-        },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        russo: ["Russo One", "sans-serif"],
+        saira: ["Saira", "sans-serif"],
+      },
+      colors: {
+        primary: "#E76F51",
+        secondary: "#5C646C",
+        light: "#F4F4F4",
+        dark: "#212529",
       },
     },
-   
-        // ... other configurations
-        plugins: [
-          require('daisyui'),
-          // ... other plugins
-        ],
-  
-  };
-  
+  },
+  plugins: [
+    daisyui,
+    // ... other plugins
+  ],
+};
