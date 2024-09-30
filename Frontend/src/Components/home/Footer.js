673960a085a9969b092e5318e87cfeb1e3c32e2f
GehanFonseka <gehanfonseka@gmail.com>
2024-09-30 16:32:14 +0530

    Update Footer.js

--- Frontend/src/Components/home/Footer.js
+++ Frontend/src/Components/home/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
           <FooterLink href="#">CLOTHING</FooterLink>
           <FooterLink href="#">SALOON</FooterLink>
           <FooterLink href="/DashboardContainer">ACCESSORIES</FooterLink>
-          <FooterLink href="#">HELP</FooterLink>
+          <FooterLink href="/Overview">HELP</FooterLink>
 
         </FooterColumn>
 
