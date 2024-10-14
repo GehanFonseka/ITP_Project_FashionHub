94c92a318405c6beb7fe42eb5534eb6e22fb9f4c
Viduranaga MAO <vidurangaomash@gmail.com>
2024-10-14 20:27:29 +0530

    oms

--- Frontend/src/Components/Finance/DisplayReport.js
+++ Frontend/src/Components/Finance/DisplayReport.js
@@ -240,7 +240,7 @@ const DisplayReport = () => {
               e.stopPropagation(); 
               handleCardClick(report);
             }}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2"
+            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2"
           >
             View
           </button>
@@ -249,7 +249,7 @@ const DisplayReport = () => {
               e.stopPropagation();
               handleEditClick(report._id);
             }}
-            className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 mr-2"
+            className="bg-primary text-white px-4 py-2 rounded hover:bg-yellow-600 mr-2"
           >
             Edit
           </button>
